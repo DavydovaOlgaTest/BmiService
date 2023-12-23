515678e40febf25bea00326884382977a00f105d
Davydova Olga test <davydovaolga.test@gmail.com>
2023-12-23 15:53:14 +0300

    Code fix

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,9 @@
 public class BmiService {
     public int calculate(int weightKg, double growthMeter) {
-        double index;
-        index = (weightKg / (growthMeter * growthMeter));
+        double index = (weightKg / (growthMeter * growthMeter));
         return (int) index;
     }
 }
+/* double index = weightKg / growthMeter / growthMeter;
+return (int) index;git 
+ */
